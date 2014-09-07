8925a87da409f27f4d33620559f87d6916325f12
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2014-09-07 15:21:43 +0200

    Jumping sheep.

--- test_filter.py
+++ test_filter.py
@@ -5,10 +5,14 @@ import sys
 import struct
 import StringIO
 import cairo
+import rsvg
+import math
 
 from imgio import read_frame, write_frame, get_cairo_context
 
-def edit_frame(ctx, size):
+sheep_svg = rsvg.Handle("pecora.svg")
+
+def edit_frame(ctx, size, num):
     ctx.scale (size[0]/4, size[1]/3)
     ctx.set_source_rgb(0.0, 1.0, 0.0)
     ctx.select_font_face("Ubuntu Medium",
@@ -21,15 +25,22 @@ def edit_frame(ctx, size):
     ctx.move_to(0.1, 0.1 - y_bearing)
     ctx.show_text(text)
 
+    ctx.identity_matrix()
+    ctx.translate(2.0 * num, 350.0 + 50.0 * math.cos(2 * math.pi * num / 200.0))
+    ctx.scale(0.25, 0.25)
+    sheep_svg.render_cairo(ctx)
+
 def main():
+    num = 0
     try:
         while True:
             image = read_frame(sys.stdin)
             #print >> sys.stderr, image.shape
             ctx, size, surf = get_cairo_context(image)
-            edit_frame(ctx, size)
+            edit_frame(ctx, size, num)
             surf.flush()
             write_frame(sys.stdout, image)
+            num += 1
     except KeyboardInterrupt:
         pass
 
