7685c406e25feb160540205e5a30ef1de08b17dd
Alessandro Achille <alessandro.achi@gmail.com>
2014-10-17 21:10:01 +0200

    Aggiunta la parte per renderizzare pagine web.

---
+++ filter_draw_image.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import struct
+import StringIO
+import cairo
+import rsvg
+import math
+
+from imgio import read_frame, write_frame, get_cairo_context
+
+sheep_svg = rsvg.Handle("pecora.svg")
+fweb = open("webpage_fifo", "r")
+
+def edit_frame(ctx, size, num):
+    global fweb
+    image_to_draw = read_frame(fweb)
+    a, b, web_render = get_cairo_context(image_to_draw)
+    #image_to_draw=cairo.ImageSurface.create_from_png("webpage_render.png")
+    ctx.save()
+    ctx.set_source_surface(web_render)
+    ctx.paint()
+    ctx.restore()
+    
+def main():
+    fweb = open("webpage_fifo", "r")
+    num = 0
+    try:
+        while True:
+            image = read_frame(sys.stdin)
+            
+            #print >> sys.stderr, image.shape
+            ctx, size, surf = get_cairo_context(image)
+            edit_frame(ctx, size, num)
+            surf.flush()
+            write_frame(sys.stdout, image)
+            num += 1
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
---
+++ phantomtest.js
@@ -0,0 +1,60 @@
+var system = require('system');
+var page = require('webpage').create();
+
+var isPageLoaded=false;
+
+
+
+page.open('images/4gears.svg', function() {
+    t=0;
+    
+    /*
+    render_function =  function() {
+	//page.render('/dev/stdout',{format: 'jpg'});
+	system.stderr.write(' '+t++);
+	if (t>400) {phantom.exit();}
+	setTimeout(render_function, 1);
+    }
+    render_function();
+    */    
+    
+    interval = setInterval(function(){
+	page.render('/dev/stdout',{format: 'jpg'});
+	system.stderr.write(' '+t++);
+	//if (t>400) {phantom.exit();}
+    }, 10);
+    
+});
+
+
+
+
+/*
+    interval = setInterval(function(){
+	page.render('/dev/stdout',{format: 'jpg'});
+	system.stderr.write(' '+t++);
+	//if (t>400) {phantom.exit();}
+    }, 10);
+*/
+
+/*
+function pack_uint32(num){
+  var s='';
+  
+  var n = num & 0xffffffff;
+
+  s+=String.fromCharCode((n & 0xff000000) >>> 24);
+  s+=String.fromCharCode((n & 0x00ff0000) >>> 16);
+  s+=String.fromCharCode((n & 0x0000ff00) >>>  8);
+  s+=String.fromCharCode((n & 0x000000ff));
+  
+  return s;
+}
+*/
+
+//data=atob(page.renderBase64());
+//data=page.renderBase64();
+//system.stdout.write(pack_uint32(data.length));
+//system.stdout.write(data);
+//page.render('./test_true.png');
+//console.log(page.renderBase64('BMP'));
