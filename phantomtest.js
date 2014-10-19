d6ad6bf1198e92abb245043e429d51c5d44d9841
Alessandro Achille <alessandro.achi@gmail.com>
2014-10-19 16:34:18 +0200

    Un modello di come potrebbero essere le animazioni.
    Funziona solo su Chrome (Firefox ha un bug, ma anche un workaround, IE non ne parliamo).
    Chrome non sa applicare trasformazioni 3d a del testo svg, da cui il workaround tremendo per il testo.

--- phantomtest.js
+++ phantomtest.js
@@ -3,9 +3,21 @@ var page = require('webpage').create();
 
 var isPageLoaded=false;
 
+function pack_uint32(num){
+  var s='';
+  
+  var n = num & 0xffffffff;
+
+  s+=String.fromCharCode((n & 0xff000000) >>> 24);
+  s+=String.fromCharCode((n & 0x00ff0000) >>> 16);
+  s+=String.fromCharCode((n & 0x0000ff00) >>>  8);
+  s+=String.fromCharCode((n & 0x000000ff));
+  
+  return s;
+}
 
 page.viewportSize = { width: 1280, height: 720 };
-page.open('images/4gears.svg', function() {
+page.open('http://tympanus.net/Development/SVGDrawingAnimation/index3.html', function() {
     t=0;
     
     /*
@@ -19,9 +31,14 @@ page.open('images/4gears.svg', function() {
     */    
     
     interval = setInterval(function(){
-	page.render('/dev/stdout',{format: 'raw'});
+	page.render('/dev/null',{format: 'ppm'});
+	//page.render('test.ppm');
 	system.stderr.write(' '+t++);
-	//if (t>400) {phantom.exit();}
+	if (t>400) {phantom.exit();}
+	//system.stdout.write(pack_uint32(page.content.length));
+	//system.stdout.write(page.content);
+	//console.log(page.content.length);
+	//console.log(page.content);
     }, 10);
     
 });
@@ -37,20 +54,9 @@ page.open('images/4gears.svg', function() {
     }, 10);
 */
 
-/*
-function pack_uint32(num){
-  var s='';
-  
-  var n = num & 0xffffffff;
 
-  s+=String.fromCharCode((n & 0xff000000) >>> 24);
-  s+=String.fromCharCode((n & 0x00ff0000) >>> 16);
-  s+=String.fromCharCode((n & 0x0000ff00) >>>  8);
-  s+=String.fromCharCode((n & 0x000000ff));
-  
-  return s;
-}
-*/
+
+
 
 //data=atob(page.renderBase64());
 //data=page.renderBase64();
--- test_filter.py
+++ test_filter.py
@@ -10,25 +10,44 @@ import math
 
 from imgio import read_frame, write_frame, get_cairo_context, TJPF_BGRX
 
-sheep_svg = rsvg.Handle("pecora.svg")
+#sheep_svg = rsvg.Handle("pecora.svg")
+
+fin = open('webpage_fifo', 'r')
+
+def read_svg_frame():
+    global fin
+    length=int(fin.readline().strip())
+    #length, = struct.unpack("!I", length_tag)
+    #print >> sys.stderr, "length ",length
+    return fin.read(length+1)
 
 def edit_frame(ctx, size, num):
-    ctx.scale (size[0]/4, size[1]/3)
-    ctx.set_source_rgb(0.0, 1.0, 0.0)
-    ctx.select_font_face("Ubuntu Medium",
-                         cairo.FONT_SLANT_NORMAL,
-                         cairo.FONT_WEIGHT_NORMAL)
-    ctx.set_font_size(0.12)
-    text = "MATEMATICI 1874 1110 FISICI"
-    x_bearing, y_bearing, width, height = ctx.text_extents(text)[:4]
-    # ctx.move_to(0.5 - width / 2 - x_bearing, 0.5 - height / 2 - y_bearing)
-    ctx.move_to(0.1, 0.1 - y_bearing)
-    ctx.show_text(text)
+ #   ctx.scale (size[0]/4, size[1]/3)
+ #   ctx.set_source_rgb(0.0, 1.0, 0.0)
+ #   ctx.select_font_face("Ubuntu Medium",
+ #                        cairo.FONT_SLANT_NORMAL,
+ #                        cairo.FONT_WEIGHT_NORMAL)
+ #   ctx.set_font_size(0.12)
+ #   text = "MATEMATICI 1874 1110 FISICI"
+ #   x_bearing, y_bearing, width, height = ctx.text_extents(text)[:4]
+ #   # ctx.move_to(0.5 - width / 2 - x_bearing, 0.5 - height / 2 - y_bearing)
+ #   ctx.move_to(0.1, 0.1 - y_bearing)
+ #   ctx.show_text(text)
 
+    ctx.move_to(0.1, 0.1)
     ctx.identity_matrix()
-    ctx.translate(2.0 * num, 350.0 + 50.0 * math.cos(2 * math.pi * num / 200.0))
-    ctx.scale(0.25, 0.25)
-    sheep_svg.render_cairo(ctx)
+ 
+ #   ctx.translate(2.0 * num, 350.0 + 50.0 * math.cos(2 * math.pi * num / 200.0))
+ #   ctx.scale(0.25, 0.25)
+    
+    svg_code=read_svg_frame()
+    #print >> sys.stderr, svg_code
+    svg_handle = rsvg.Handle(data=svg_code)
+    #svg_handle.write(buffer=svg_code)
+    
+    #svg_handle = rsvg.Handle('images/4gears.svg')
+    
+    svg_handle.render_cairo(ctx)
 
 def main():
     num = 0
