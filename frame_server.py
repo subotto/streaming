10113d7aeac35e9f6c86855b6c48404a5415bf4c
Giovanni Mascellani <gio@debian.org>
2016-11-02 23:41:29 +0100

    Support rate limiting in frame server.

--- frame_server.py
+++ frame_server.py
@@ -3,6 +3,7 @@
 
 import sys
 import StringIO
+import time
 
 from net import ImageSocketServer
 from imgio import read_jpeg_frame, write_jpeg_frame
@@ -15,9 +16,19 @@ def main():
     server = ImageSocketServer(host, port)
 
     num = 0
+    time_offset = None
+    limiter = True
     try:
         while True:
             imdata, timestamp = read_jpeg_frame(sys.stdin)
+            if limiter:
+                now = time.time()
+                if time_offset is None:
+                    time_offset = now - timestamp
+                else:
+                    sleep_time = time_offset + timestamp - now
+                    if sleep_time > 0:
+                        time.sleep(sleep_time)
             tmp = StringIO.StringIO()
             write_jpeg_frame(tmp, imdata, timestamp)
             server.post_data(tmp.getvalue())
