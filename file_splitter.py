a7baa5bc57fd56cfaec6ebdb7592174d3f123caa
Giovanni Mascellani <gio@debian.org>
2016-11-04 17:58:57 +0100

    Start on a later chunk and use fadvise for better cache optimization.

--- file_splitter.py
+++ file_splitter.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import fadvise
 
 BUF_SIZE = 1024 * 1024
 
@@ -21,6 +23,8 @@ def copy(fin, fout, l):
 def main():
     orig_file = sys.argv[1]
     new_model = sys.argv[2]
+    if len(sys.argv) > 3:
+        start_chunk = int(sys.argv[3])
 
     forig = open(orig_file)
     prev_pos = 0
@@ -29,8 +33,15 @@ def main():
         pos = int(line.strip())
         diff = pos - prev_pos
         assert diff >= 0
-        with open(new_model % (new_num), 'w') as fdest:
-            copy(forig, fdest, diff)
+        if new_num >= start_chunk:
+            filename = new_model % (new_num)
+            with open(new_model % (new_num), 'w') as fdest:
+                fadvise.posix_fadvise(forig.fileno(), 0, forig.tell(), fadvise.POSIX_FADV_DONTNEED)
+                fadvise.posix_fadvise(forig.fileno(), forig.tell(), 0, fadvise.POSIX_FADV_SEQUENTIAL)
+                fadvise.posix_fadvise(fdest.fileno(), 0, 0, fadvise.POSIX_FADV_SEQUENTIAL)
+                copy(forig, fdest, diff)
+        else:
+            forig.seek(diff, os.SEEK_CUR)
         prev_pos = pos
         new_num += 1
 
